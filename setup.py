02d4aa160c9c0b2f55c08ef79733b09bc1141d2a
Christophe Bedard <fixed-term.christophe.bourquebedard@de.bosch.com>
2019-06-06 09:17:36 +0200

    Make package a ROS 2 package

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+package_name = 'tracetools_analysis'
+
+setup(
+    name=package_name,
+    version='0.1.0',
+    packages=[package_name],
+    data_files=[
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    keywords=['ROS'],
+    description='Tools for analysing trace data',
+    entry_points={
+        'console_scripts': [
+            f'convert = {package_name}.convert:main',
+        ],
+    },
+)
